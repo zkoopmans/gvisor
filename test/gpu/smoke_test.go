836dadbde87826c9b2cf8a7bde3f280deddff20d
Zach Koopmans <zkoopmans@google.com>
2025-06-30 22:24:29 +0000

    Update smoke-tests to work on ARM.

--- test/gpu/smoke_test.go
+++ test/gpu/smoke_test.go
@@ -17,6 +17,7 @@ package smoke_test
 
 import (
 	"context"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -48,6 +49,9 @@ func TestNvidiaSmi(t *testing.T) {
 
 func TestGPUHello(t *testing.T) {
 	ctx := context.Background()
+	if strings.Contains(runtime.GOARCH, "arm64"){
+        t.Skipf("Test does not run on ARM")
+    }
 	runGPUHello(ctx, t, "gpu/cuda-tests")
 }
 
